1d27e6f748f3868ff1f6369749f88ab4809d16b9
emuriqi <elionm@uia.no>
2023-09-27 15:53:47 +0200

    nå funker updater profil

--- update_profile.php
+++ update_profile.php
@@ -1,76 +1,113 @@
-<?php 
+<?php
+
 include 'config.php';
 session_start();
+$id = $_SESSION['id'];
 
-// Check if user_id is set in the session
-if(isset($_SESSION['id'])){
-    $user_id = $_SESSION['id'];
+if(isset($_POST['update_profile'])){
 
-    if(isset($_POST['update_profile'])){
-        $update_name = mysqli_real_escape_string($conn, $_POST['update_name']);
-        $update_email = mysqli_real_escape_string($conn, $_POST['update_email']);
+   $update_name = mysqli_real_escape_string($conn, $_POST['update_name']);
+   $update_email = mysqli_real_escape_string($conn, $_POST['update_email']);
 
-        mysqli_query($conn, "UPDATE `user_form` SET name = '$update_name', email = '$update_email' WHERE id = '$user_id'") or die('query failed');
-    }
-} else {
-    // Handle the case where user_id is not set in the session
-    echo "Session id is not set.";
+   mysqli_query($conn, "UPDATE `user_form` SET name = '$update_name', email = '$update_email' WHERE id = '$id'") or die('query failed');
+
+   $old_pass = $_POST['old_pass'];
+   $update_pass = mysqli_real_escape_string($conn, md5($_POST['update_pass']));
+   $new_pass = mysqli_real_escape_string($conn, md5($_POST['new_pass']));
+   $confirm_pass = mysqli_real_escape_string($conn, md5($_POST['confirm_pass']));
+
+   if(!empty($update_pass) || !empty($new_pass) || !empty($confirm_pass)){
+      if($update_pass != $old_pass){
+         $message[] = 'old password not matched!';
+      }elseif($new_pass != $confirm_pass){
+         $message[] = 'confirm password not matched!';
+      }else{
+         mysqli_query($conn, "UPDATE `user_form` SET password = '$confirm_pass' WHERE id = '$id'") or die('query failed');
+         $message[] = 'password updated successfully!';
+      }
+   }
+
+   $update_image = $_FILES['update_image']['name'];
+   $update_image_size = $_FILES['update_image']['size'];
+   $update_image_tmp_name = $_FILES['update_image']['tmp_name'];
+   $update_image_folder = 'uploaded_img/'.$update_image;
+
+   if(!empty($update_image)){
+      if($update_image_size > 2000000){
+         $message[] = 'image is too large';
+      }else{
+         $image_update_query = mysqli_query($conn, "UPDATE `user_form` SET image = '$update_image' WHERE id = '$id'") or die('query failed');
+         if($image_update_query){
+            move_uploaded_file($update_image_tmp_name, $update_image_folder);
+         }
+         $message[] = 'image updated succssfully!';
+      }
+   }
+
 }
+
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Update Profile</title>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>update profile</title>
 
-    <!-- custom css file link  -->
-   <link rel="stylesheet" href="css/update_profile.css">
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/updateProfile.css">
+
 </head>
 <body>
-   <div class="update_profile">
+   
+<div class="update-profile">
 
    <?php
-      $fetch = array(); // Initialize $fetch as an empty array
-      $select = mysqli_query($conn, "SELECT * FROM `user_form` WHERE id = '$user_id'")
-      or die('query failed');
+      $select = mysqli_query($conn, "SELECT * FROM `user_form` WHERE id = '$id'") or die('query failed');
       if(mysqli_num_rows($select) > 0){
-        $fetch = mysqli_fetch_assoc($select);
+         $fetch = mysqli_fetch_assoc($select);
       }
    ?>
 
-    <form action="update_profile.php" method="post" enctype="multipart/form-data">
-        <?php
-        if($fetch['image'] == ''){
+   <form action="" method="post" enctype="multipart/form-data">
+      <?php
+         if($fetch['image'] == ''){
             echo '<img src="images/default-avatar.png">';
-         }else {
+         }else{
             echo '<img src="uploaded_img/'.$fetch['image'].'">';
          }
-        ?>
+         if(isset($message)){
+            foreach($message as $message){
+               echo '<div class="message">'.$message.'</div>';
+            }
+         }
+      ?>
+      <div class="flex">
+         <div class="inputBox">
+            <span>username :</span>
+            <input type="text" name="update_name" value="<?php echo $fetch['name']; ?>" class="box">
+            <span>your email :</span>
+            <input type="email" name="update_email" value="<?php echo $fetch['email']; ?>" class="box">
+            <span>update your pic :</span>
+            <input type="file" name="update_image" accept="image/jpg, image/jpeg, image/png" class="box">
+         </div>
+         <div class="inputBox">
+            <input type="hidden" name="old_pass" value="<?php echo $fetch['password']; ?>">
+            <span>old password :</span>
+            <input type="password" name="update_pass" placeholder="enter previous password" class="box">
+            <span>new password :</span>
+            <input type="password" name="new_pass" placeholder="enter new password" class="box">
+            <span>confirm password :</span>
+            <input type="password" name="confirm_pass" placeholder="confirm new password" class="box">
+         </div>
+      </div>
+      <input type="submit" value="update profile" name="update_profile" class="btn">
+      <a href="home.php" class="delete-btn">go back</a>
+   </form>
 
-         <div class="flex">
-         <div class="inputbox">
-         <span>username:</span>
-         <input type="text" name="update_name" value="<?php echo $fetch['name']?>" class="box">
-         <span>your email :</span>
-         <input type="email" name="update_email" value="<?php echo $fetch['email']?>" class="box">
-         <span>update your pic:</span>
-         <input type="file" name="update_image"  class="box" accept="image/jpg, image/jpeg, image/png">
-    </div>
-         <div class="inputbox">
-         <input type="hidden" name="old_name" value="<?php echo $fetch['password']?>">
-         <span>old password :</span>
-         <input type="password" name="update_pass" placeholder="enter previous password" class="box">
-         <span>new password :</span>
-         <input type="password" name="new_pass" placeholder="enter new password" class="box">
-         <span>confirm password :</span>
-         <input type="password" name="confirm_pass" placeholder="confirm new password" class="box">
-     </div>
-     </div>
-         <input type="submit" value="update profile" name="update_profile">
-         <a href="home.php" class="delete-btn">go back</a>
-     </form>
-   </div>
+</div>
+
 </body>
 </html>
-
